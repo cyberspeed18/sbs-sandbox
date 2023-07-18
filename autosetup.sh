16192beea8d468b2421de418e160a135207deac4
Natthaku <natthaku@hotmail.com>
2023-07-19 06:21:49 +0700

    no message

---
+++ autosetup.sh
@@ -0,0 +1,8 @@
+#Install
+cd / && mkdir -p airflow && cd airflow &&
+curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.6.3/docker-compose.yaml' &&
+echo -e "AIRFLOW_UID=$(id -u)" > .env &&
+ls -ls &&
+export AIRFLOW_PROJ_DIR=C:/DockerData/Airflow/ &&
+echo $AIRFLOW_PROJ_DIR &&
+docker compose up airflow-init -d && docker compose up -d && sleep 30s && docker-compose ps &&
