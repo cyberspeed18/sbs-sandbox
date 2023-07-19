a0ed97e621cf66d2f58778e865fe1f3d6d42fd9c
Natthaku <natthaku@hotmail.com>
2023-07-19 07:25:32 +0700

    submit

--- setup_airflow/setup.sh
+++ setup_airflow/setup.sh
@@ -5,5 +5,5 @@ echo -e "AIRFLOW_UID=$(id -u)" > .env &&
 ls -ls &&
 export AIRFLOW_PROJ_DIR=C:/DockerData/Airflow/ &&
 echo $AIRFLOW_PROJ_DIR &&
-docker compose up airflow-init -d && docker compose up -d && sleep 30s && docker-compose ps &&
+docker compose up airflow-init -d && docker compose up -d && sleep 3m && docker-compose ps &&
 echo Airflow installation completed.
